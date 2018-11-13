a058732ff8a8db9935421e28101d26f3b9270c74
LuzCappuccino <luz.camacho323@gmail.com>
2018-11-13 02:11:55 -0800

    retrival class and conflict func written

--- src/csci201finalproject/Section.java
+++ src/csci201finalproject/Section.java
@@ -20,31 +20,56 @@ public class Section {
 	public String getSectionID() {
 		return SectionID;
 	}
+	
 	public void setSectionID(String sectionID) {
 		this.SectionID = sectionID;
 	}
+	
 	public String getType() {
 		return Type;
 	}
+	
 	public void setType(String type) {
 		this.Type = type;
 	}
+	
 	public String getStartTime() {
 		return StartTime;
 	}
+	
 	public void setStartTime(String startTime) {
 		this.StartTime = startTime;
 	}
+	
 	public String getEndTime() {
 		return EndTime;
 	}
+	
 	public void setEndTime(String endTime) {
 		this.EndTime = endTime;
 	}
+	
 	public ArrayList<Integer> getDays() {
 		return Days;
 	}
+	
 	public void setDays(ArrayList<Integer> days) {
 		this.Days = days;
+	}
+	
+	public boolean doesConflict(String otherEndTime, String otherStartTime, ArrayList<Integer> otherDays) {
+		for(int i = 0; i < this.Days.size(); i++) {
+			for(int k = 0; k < otherDays.size(); k++) {
+				// identify potential conflict 
+				if(otherDays.get(k) == this.Days.get(i)) {
+					if(this.StartTime.compareTo(otherStartTime) == 0) return true;
+					if(this.EndTime.compareTo(otherEndTime) == 0) return true;
+					// if the otherStarTime is greater than the startTime but less than the end time => conflict 
+					if(this.StartTime.compareTo(otherStartTime) < 0 && this.EndTime.compareTo(otherStartTime) > 0) return true;
+					if(this.StartTime.compareTo(otherEndTime) < 0 && this.EndTime.compareTo(otherEndTime) > 0) return true;
+				}
+			}
+		}
+		return false;
 	}
 }
